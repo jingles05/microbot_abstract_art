340f96302eafdcca3efec834bfeeef449b3e68e8
Jingles05 <102975990+jingles05@users.noreply.github.com>
2022-04-21 14:14:00 -0600

    deleted some things

--- main.ts
+++ main.ts
@@ -9,9 +9,6 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     pins.analogWritePin(AnalogPin.P0, 10)
     pins.analogWritePin(AnalogPin.P1, 100)
-    basic.pause(100)
-    pins.analogWritePin(AnalogPin.P0, 100)
-    pins.analogWritePin(AnalogPin.P1, 100)
 })
 pins.analogWritePin(AnalogPin.P0, 100)
 pins.analogWritePin(AnalogPin.P1, 100)
