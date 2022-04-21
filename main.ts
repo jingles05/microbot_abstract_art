9927d6bebb478c5037e87cb2db2557dcd6a0b06a
Jingles05 <102975990+jingles05@users.noreply.github.com>
2022-04-21 14:10:21 -0600

    made a straight line

--- main.ts
+++ main.ts
@@ -6,11 +6,15 @@ input.onButtonPressed(Button.A, function () {
     pins.analogWritePin(AnalogPin.P0, 100)
     basic.pause(300)
 })
+input.onButtonPressed(Button.B, function () {
+    pins.analogWritePin(AnalogPin.P0, 10)
+    pins.analogWritePin(AnalogPin.P1, 100)
+    basic.pause(100)
+    pins.analogWritePin(AnalogPin.P0, 100)
+    pins.analogWritePin(AnalogPin.P1, 100)
+})
 pins.analogWritePin(AnalogPin.P0, 100)
 pins.analogWritePin(AnalogPin.P1, 100)
 basic.pause(100)
 pins.analogWritePin(AnalogPin.P0, 100)
 pins.analogWritePin(AnalogPin.P0, 100)
-basic.forever(function () {
-	
-})
